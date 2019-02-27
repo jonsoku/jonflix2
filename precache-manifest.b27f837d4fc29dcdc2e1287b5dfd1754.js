66cfafeffcbdab7cd579280af67a1522db512087
Jongseok Lee <jongseoklee@Jongseokui-iMac.local>
2019-02-27 19:26:39 +0900

    Updates

---
+++ precache-manifest.b27f837d4fc29dcdc2e1287b5dfd1754.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "74e8143c9871c0b17bc58788cf479708",
+    "url": "/jonflix2/static/media/noPosterSmall.74e8143c.png"
+  },
+  {
+    "revision": "a608e2b6314df8c91db0",
+    "url": "/jonflix2/static/js/runtime~main.a608e2b6.js"
+  },
+  {
+    "revision": "96182c844d2b788706db",
+    "url": "/jonflix2/static/js/main.96182c84.chunk.js"
+  },
+  {
+    "revision": "23bb8f5ed4820767571c",
+    "url": "/jonflix2/static/js/2.23bb8f5e.chunk.js"
+  },
+  {
+    "revision": "5d16279336da54a091840ac32d776c42",
+    "url": "/jonflix2/index.html"
+  }
+];
